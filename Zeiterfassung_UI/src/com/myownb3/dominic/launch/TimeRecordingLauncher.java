6f2224041d638b9ae0058f0fd054a940adf8c2fb
Dominic Stalder <dominic.stalder89@gmail.com>
2019-05-13 08:49:27 +0200

    b add launch-methode in order to start

--- Zeiterfassung_UI/src/com/myownb3/dominic/launch/TimeRecordingLauncher.java
+++ Zeiterfassung_UI/src/com/myownb3/dominic/launch/TimeRecordingLauncher.java
@@ -19,6 +19,9 @@ import javafx.stage.Stage;
  */
 public class TimeRecordingLauncher extends Application {
 
+    public static void main(String[] args) {
+        launch();
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
