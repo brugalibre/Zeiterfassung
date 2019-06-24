013389696ac71eacab6021f7014561b475b71b02
Dominic Stalder <dominic.stalder89@gmail.com>
2019-06-24 08:01:00 +0200

    F Create empty TurobBucher.properties file if not exist

--- Zeiterfassung_UI/src/com/myownb3/dominic/launch/TimeRecordingLauncher.java
+++ Zeiterfassung_UI/src/com/myownb3/dominic/launch/TimeRecordingLauncher.java
@@ -3,6 +3,11 @@
  */
 package com.myownb3.dominic.launch;
 
+import static com.myownb3.dominic.timerecording.settings.common.Const.TURBO_BUCHER_PROPERTIES;
+
+import java.io.File;
+import java.io.IOException;
+
 import com.myownb3.dominic.launch.exception.ApplicationLaunchException;
 import com.myownb3.dominic.librarys.pictures.PictureLibrary;
 import com.myownb3.dominic.timerecording.app.Message;
@@ -35,9 +40,25 @@ public class TimeRecordingLauncher extends Application {
 	TimeRecorder.setCallbackHandler(callbackHandler);
 	GlobalExceptionHandler.registerHandler(callbackHandler);
 
+	initApplication();
+	Platform.runLater(() -> timeRecordingTray.registerSystemtray(primaryStage));
+    }
+
+    private void initApplication() {
 	PictureLibrary.loadPictures();
 	Platform.setImplicitExit(false);
-	Platform.runLater(() -> timeRecordingTray.registerSystemtray(primaryStage));
+	createTurboBucherPropertiesFileIfNotExists();
+    }
+
+    private void createTurboBucherPropertiesFileIfNotExists() {
+	File file = new File (TURBO_BUCHER_PROPERTIES);
+	if (!file.exists()){
+	    try {
+		file.createNewFile();
+	    } catch (IOException e) {
+		throw new ApplicationLaunchException(e);
+	    }
+	}
     }
 
     /**
