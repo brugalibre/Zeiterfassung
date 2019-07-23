db6aabc73e3d029e4c9575021b9716847371f158
Dominic Stalder <dominic.stalder89@gmail.com>
2019-07-23 15:50:30 +0200

    r moved methods from BusinessDayChangedCallbackHandlerImpl to BusinessDay itself

--- Zeiterfassung_App/src/com/myownb3/dominic/timerecording/work/businessday/BusinessDayChangedCallbackHandlerImpl.java
+++ Zeiterfassung_App/src/com/myownb3/dominic/timerecording/work/businessday/BusinessDayChangedCallbackHandlerImpl.java
@@ -1,12 +1,9 @@
 package com.myownb3.dominic.timerecording.work.businessday;
 
-import java.util.Optional;
-
 import com.myownb3.dominic.timerecording.app.TimeRecorder;
 import com.myownb3.dominic.timerecording.callback.handler.BusinessDayChangedCallbackHandler;
 import com.myownb3.dominic.timerecording.callback.handler.impl.BusinessDayIncrementAdd;
 import com.myownb3.dominic.timerecording.callback.handler.impl.ChangedValue;
-import com.myownb3.dominic.timerecording.charge.InvalidChargeTypeRepresentationException;
 
 public class BusinessDayChangedCallbackHandlerImpl implements BusinessDayChangedCallbackHandler {
 
@@ -26,43 +23,6 @@ public class BusinessDayChangedCallbackHandlerImpl implements BusinessDayChanged
     public void handleBusinessDayChanged(ChangedValue changeValue) {
 
 	BusinessDay businessDay = TimeRecorder.INSTANCE.getBussinessDay();
-
-	Optional<BusinessDayIncrement> businessDayIncOpt = businessDay
-		.getBusinessIncrement(changeValue.getSequence());
-	businessDayIncOpt.ifPresent(businessDayIncrement -> {
-	    handleBusinessDayChangedInternal(changeValue, businessDay, businessDayIncrement);
-	});
-    }
-
-    private void handleBusinessDayChangedInternal(ChangedValue changedValue, BusinessDay businessDay,
-	    BusinessDayIncrement businessDayIncremental) {
-
-	switch (changedValue.getValueTypes()) {
-	case DESCRIPTION:
-	    businessDayIncremental.setDescription(changedValue.getNewValue());
-	    break;
-	case BEGIN:
-	    businessDayIncremental.updateBeginTimeSnippetAndCalculate(businessDayIncremental,
-		    changedValue.getFromUptoSequence(), changedValue.getNewValue());
-	    break;
-	case END:
-	    businessDayIncremental.updateEndTimeSnippetAndCalculate(businessDayIncremental,
-		    changedValue.getFromUptoSequence(), changedValue.getNewValue());
-	    break;
-	case TICKET_NR:
-	    businessDayIncremental.setTicketNumber(changedValue.getNewValue());
-	    break;
-	case CHARGE_TYPE:
-	    try {
-		businessDayIncremental.setChargeType(changedValue.getNewValue());
-	    } catch (InvalidChargeTypeRepresentationException e) {
-		e.printStackTrace();
-		// ignore failures
-	    }
-	    break;
-	default:
-	    throw new UnsupportedOperationException ("ChargeType '" + changedValue.getValueTypes() + "' not implemented!");
-	}
-	businessDay.checkForRedundancys();
+	businessDay.changeBusinesDayIncrement(changeValue);
     }
 }
