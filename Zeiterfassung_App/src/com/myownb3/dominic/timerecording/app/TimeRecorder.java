0f3d0a152cc14d6089b23613b7a671dade693119
Dominic Stalder <dominic.stalder89@gmail.com>
2019-07-23 15:50:38 +0200

    r code formatting

--- Zeiterfassung_App/src/com/myownb3/dominic/timerecording/app/TimeRecorder.java
+++ Zeiterfassung_App/src/com/myownb3/dominic/timerecording/app/TimeRecorder.java
@@ -30,16 +30,16 @@ public class TimeRecorder {
      * The singleton instance of this class
      */
     public static final TimeRecorder INSTANCE = new TimeRecorder();
-    
+
     /**
      * The version of the application
      */
     public static final String VERSION = "1.4.6";
-  
+
     private BusinessDay businessDay;
     private CallbackHandler callbackHandler;
     private WorkStates currentState;
-    private TIME_TYPE timeType; 
+    private TIME_TYPE timeType;
 
     private TimeRecorder() {
 	timeType = TIME_TYPE.HOUR;
