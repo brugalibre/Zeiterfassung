7a2a3aa0aaa7cf518742f2ce064f7cd6058dd518
Dominic <Dominic@Galaxy-Book>
2019-07-28 15:33:08 +0200

    new version (since we can start/stop with hotkey & show dialog error)

--- Zeiterfassung_App/src/com/myownb3/dominic/timerecording/app/TimeRecorder.java
+++ Zeiterfassung_App/src/com/myownb3/dominic/timerecording/app/TimeRecorder.java
@@ -34,7 +34,7 @@ public class TimeRecorder {
     /**
      * The version of the application
      */
-    public static final String VERSION = "1.5.0";
+    public static final String VERSION = "1.5.1";
 
     private BusinessDay businessDay;
     private CallbackHandler callbackHandler;
