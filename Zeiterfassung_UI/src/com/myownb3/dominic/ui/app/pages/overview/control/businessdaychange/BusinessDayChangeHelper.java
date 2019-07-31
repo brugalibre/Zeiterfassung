31afc4d5567da155e13dee5b0de7400a353617fd
Dominic <Dominic@Piranha>
2019-07-31 15:54:15 +0200

    r moved the methods to change a businessdayinc into a helper

---
+++ Zeiterfassung_UI/src/com/myownb3/dominic/ui/app/pages/overview/control/businessdaychange/BusinessDayChangeHelper.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package com.myownb3.dominic.ui.app.pages.overview.control.businessdaychange;
+
+import com.myownb3.dominic.timerecording.core.callbackhandler.BusinessDayChangedCallbackHandler;
+import com.myownb3.dominic.timerecording.core.callbackhandler.impl.BusinessDayChangedCallbackHandlerImpl;
+import com.myownb3.dominic.timerecording.core.callbackhandler.impl.ChangedValue;
+import com.myownb3.dominic.timerecording.core.work.businessday.BusinessDay;
+import com.myownb3.dominic.timerecording.core.work.businessday.BusinessDayIncrement;
+import com.myownb3.dominic.timerecording.core.work.businessday.ValueTypes;
+import com.myownb3.dominic.ui.app.pages.overview.control.OverviewController;
+import com.myownb3.dominic.ui.app.pages.overview.model.table.BusinessDayIncTableRowValue;
+import com.myownb3.dominic.ui.app.pages.overview.model.table.TimeSnippetCellValue;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.TableColumn.CellEditEvent;
+import javafx.scene.control.TablePosition;
+
+/**
+ * 
+ * The {@link BusinessDayChangeHelper} helps to change a
+ * {@link BusinessDayIncrement}. It listens on the table cell and calls the
+ * action to change the underlying {@link BusinessDay} if the values
+ * of the table-view changes
+ * 
+ * @author Dominic
+ * 
+ */
+public class BusinessDayChangeHelper implements EventHandler<CellEditEvent<BusinessDayIncTableRowValue, String>> {
+
+    private OverviewController overviewController;
+    private BusinessDayChangedCallbackHandler handler;
+
+    public BusinessDayChangeHelper(OverviewController overviewController) {
+	this.overviewController = overviewController;
+	this.handler = new BusinessDayChangedCallbackHandlerImpl();
+    }
+
+    @Override
+    public void handle(CellEditEvent<BusinessDayIncTableRowValue, String> event) {
+
+	BusinessDayIncTableRowValue businessDayIncTableCellValue = event.getRowValue();
+	TablePosition<BusinessDayIncTableRowValue, String> tablePosition = event.getTablePosition();
+	String newValue = event.getNewValue();
+
+	ValueTypes valueType = businessDayIncTableCellValue.getChangeValueTypeForIndex(tablePosition.getColumn());
+	// XXX Ugly hack. Since we had to make present values editable
+	if (valueType == ValueTypes.NONE) {
+	    overviewController.show();
+	    return;
+	}
+	int fromUptoSequence = getBeginEndSequence(businessDayIncTableCellValue, tablePosition);
+	int orderNumber = Integer.valueOf(businessDayIncTableCellValue.getNumber());
+	handler.handleBusinessDayChanged(ChangedValue.of(orderNumber, newValue, valueType, fromUptoSequence));
+
+	overviewController.show();
+    }
+
+    private int getBeginEndSequence(BusinessDayIncTableRowValue businessDayIncTableCellValue, TablePosition<BusinessDayIncTableRowValue, String> tablePosition) {
+	TimeSnippetCellValue timeSnippet4Index = businessDayIncTableCellValue.getTimeSnippe4RowIndex(tablePosition.getColumn());
+	return timeSnippet4Index != null ? timeSnippet4Index.getSequence() : -1;
+    }
+}
