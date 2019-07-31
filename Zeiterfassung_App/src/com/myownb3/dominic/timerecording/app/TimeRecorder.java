22e1d80f6dde2c3e130407a2c6dc6c9b3c3bb496
Dominic <Dominic@Piranha>
2019-07-31 16:08:52 +0200

    r new version since we can add a description now

--- Zeiterfassung_App/src/com/myownb3/dominic/timerecording/app/TimeRecorder.java
+++ Zeiterfassung_App/src/com/myownb3/dominic/timerecording/app/TimeRecorder.java
@@ -40,7 +40,7 @@ public class TimeRecorder {
     /**
      * The version of the application
      */
-    public static final String VERSION = "1.5.2";
+    public static final String VERSION = "1.5.3";
 
     private BusinessDay businessDay;
     private CallbackHandler callbackHandler;
