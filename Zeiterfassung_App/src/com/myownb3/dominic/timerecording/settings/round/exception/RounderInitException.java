1dcd6a85cf6c2c365e0158b19b31a2bee74f9758
Dominic Stalder <dominic.stalder89@gmail.com>
2019-08-05 10:48:20 +0200

    r refactor source code 2nd

--- Zeiterfassung_App/src/com/myownb3/dominic/timerecording/settings/round/exception/RounderInitException.java
+++ Zeiterfassung_App/src/com/myownb3/dominic/timerecording/settings/round/exception/RounderInitException.java
@@ -2,10 +2,6 @@ package com.myownb3.dominic.timerecording.settings.round.exception;
 
 public class RounderInitException extends RuntimeException {
 
-    public RounderInitException(String message) {
-	super(message);
-    }
-
     public RounderInitException(Exception e) {
 	super(e);
     }
--- Zeiterfassung_App/src/com/myownb3/dominic/timerecording/settings/round/exception/RounderStoreValueException.java
+++ Zeiterfassung_App/src/com/myownb3/dominic/timerecording/settings/round/exception/RounderStoreValueException.java
@@ -2,9 +2,6 @@ package com.myownb3.dominic.timerecording.settings.round.exception;
 
 public class RounderStoreValueException extends RuntimeException {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = 1L;
 
     public RounderStoreValueException(Exception e) {
