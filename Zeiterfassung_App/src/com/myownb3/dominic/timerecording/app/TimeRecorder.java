23896b0c88fef06f8ab6863c9e2c2d9df44a0801
Dominic <Dominic@Piranha>
2020-08-10 11:31:03 +0200

    b createNewFile returns false, obwohl Datei erstellt wurde

--- Zeiterfassung_App/src/com/myownb3/dominic/timerecording/app/TimeRecorder.java
+++ Zeiterfassung_App/src/com/myownb3/dominic/timerecording/app/TimeRecorder.java
@@ -44,7 +44,7 @@ public class TimeRecorder {
    /**
     * The version of the application
     */
-   public static final String VERSION = "1.5.8";
+   public static final String VERSION = "1.5.9";
 
    private BusinessDay businessDay;
    private CallbackHandler callbackHandler;
