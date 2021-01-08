54e23bc8617aa77aee2f7bda0b8bcefd15c1ac14
Dominic <Dominic@Piranha>
2021-01-08 13:07:23 +0100

    remove webdriver manager & add turbo-bucher.propertiets

--- Zeiterfassung_App/src/com/myownb3/dominic/timerecording/app/TimeRecorder.java
+++ Zeiterfassung_App/src/com/myownb3/dominic/timerecording/app/TimeRecorder.java
@@ -44,7 +44,7 @@ public class TimeRecorder {
    /**
     * The version of the application
     */
-   public static final String VERSION = "1.6.2";
+   public static final String VERSION = "1.6.3";
 
    private BusinessDay businessDay;
    private CallbackHandler callbackHandler;
