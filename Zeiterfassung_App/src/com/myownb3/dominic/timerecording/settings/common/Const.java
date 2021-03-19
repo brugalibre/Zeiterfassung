00f3c3c3ba76924d2bf09452b3b60b7f1e911965
Dominic <Dominic@Piranha>
2021-03-19 14:40:37 +0100

    F new properties file to store settings values

--- Zeiterfassung_App/src/com/myownb3/dominic/timerecording/settings/common/Const.java
+++ Zeiterfassung_App/src/com/myownb3/dominic/timerecording/settings/common/Const.java
@@ -1,5 +1,7 @@
 package com.myownb3.dominic.timerecording.settings.common;
 
+import com.myownb3.dominic.timerecording.app.TimeRecorder;
+
 /**
  * Contains some common constans for settings
  * 
@@ -16,4 +18,7 @@ public class Const {
     * The properties file for the turob-bucher
     */
    public static final String TURBO_BUCHER_PROPERTIES = "turbo-bucher.properties";
+
+   /** The properties file for the {@link TimeRecorder} app */
+   public static final String ZEITERFASSUNG_PROPERTIES = "zeiterfassung.properties";
 }
