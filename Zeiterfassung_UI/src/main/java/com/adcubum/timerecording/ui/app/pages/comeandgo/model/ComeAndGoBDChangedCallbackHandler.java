ee2baac4f909b63a98f2a8728ff66406a572da6a
DStalder <DStalder@ADC-NB30164.intern.cube.ch>
2021-08-05 18:56:44 +0200

    f ComeAndGo-Wizard: Show the last entered ticket-nr as default

--- Zeiterfassung_UI/src/main/java/com/adcubum/timerecording/ui/app/pages/comeandgo/model/ComeAndGoBDChangedCallbackHandler.java
+++ Zeiterfassung_UI/src/main/java/com/adcubum/timerecording/ui/app/pages/comeandgo/model/ComeAndGoBDChangedCallbackHandler.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 
 import com.adcubum.timerecording.core.work.businessday.BusinessDay;
 import com.adcubum.timerecording.core.work.businessday.TimeSnippet;
+import com.adcubum.timerecording.core.work.businessday.comeandgo.ComeAndGo;
 import com.adcubum.timerecording.core.work.businessday.comeandgo.ComeAndGoes;
 import com.adcubum.timerecording.core.work.businessday.comeandgo.change.ChangedComeAndGoValue;
 import com.adcubum.timerecording.core.work.businessday.update.callback.BusinessDayChangedCallbackHandler;
@@ -45,6 +46,17 @@ public class ComeAndGoBDChangedCallbackHandler implements BusinessDayChangedCall
    public TimeSnippet getCurrentTimeSnippet() {
       if (!businessDayIncrementAdds.isEmpty()) {
          return businessDayIncrementAdds.getLast().getTimeSnippet();
+      }
+      return null;
+   }
+
+   /**
+    * @return the ticket nr which was entered for the previous {@link ComeAndGo}
+    */
+   public String getTicketNrFromPrevAddedBDInc() {
+      if (!businessDayIncrementAdds.isEmpty()) {
+         BusinessDayIncrementAdd lastBusinessDayIncrementAdd = businessDayIncrementAdds.getLast();
+         return lastBusinessDayIncrementAdd.getTicket().getNr();
       }
       return null;
    }
