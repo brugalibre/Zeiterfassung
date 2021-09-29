b2e310254f42d823aee0c6243cb9fed8bee876f2
DStalder <DStalder@ADC-NB30164.intern.cube.ch>
2021-09-29 08:00:16 +0200

    r integrate turbo-bucher v6.6

--- Zeiterfassung_Core_Api/src/main/java/com/adcubum/timerecording/app/TimeRecorder.java
+++ Zeiterfassung_Core_Api/src/main/java/com/adcubum/timerecording/app/TimeRecorder.java
@@ -21,7 +21,7 @@ public interface TimeRecorder {
    /**
     * The version of the application
     */
-   public static final String VERSION = "1.8.3";
+   public static final String VERSION = "1.8.3.1";
 
    /**
     * Initializes this {@link TimeRecorder}
