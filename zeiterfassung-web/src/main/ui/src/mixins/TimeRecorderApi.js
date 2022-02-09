e54d18a706645ee9ecafd7d26bb6f8603350d8ee
std <dominic.stalder@nag.ch>
2022-02-09 22:40:25 +0100

    b add BusinessIncrement-dialog didn't disappear when closing

--- zeiterfassung-web/src/main/ui/src/mixins/TimeRecorderApi.js
+++ zeiterfassung-web/src/main/ui/src/mixins/TimeRecorderApi.js
@@ -1,34 +1,32 @@
 const timeRecorderApiUrl = "/api/v1/timerecorder";
 export default {
   name: 'timeRecorderApi',
-  data (){
+  data() {
     return {
       timeRecorderDto: '',
       ticketsFromBacklog: '',
       postErrorDetails: null,
-      postRequestOptions : {
+      postRequestOptions: {
         method: "POST",
-        headers: { "Content-Type": "application/json" }
+        headers: {"Content-Type": "application/json"}
       },
     }
   },
   methods: {
-    fetchTimeRecorderDto: function(){
+    fetchTimeRecorderDto: function () {
       fetch(timeRecorderApiUrl)
         .then(response => response.json())
-        .then(data => {
-          this.timeRecorderDto = data;
-      });
+        .then(data => this.timeRecorderDto = data);
     },
 
-    startStopRecording: function(){
+    startStopRecording: function () {
       fetch(timeRecorderApiUrl + "/startStopRecording", this.postRequestOptions)
         .then(async response => {
           // retrieve the response in two stepts: text -> JSON.parse. Thats necessary in case we receive an empty response (which leads always to an exception)
           const plainData = await response.text();
           const data = await (plainData ? JSON.parse(plainData) : {});
           // check for error response
-          if(!response.ok){
+          if (!response.ok) {
             // get error message from body or default to response status
             const error = (data && data.message) || response.status;
             this.postErrorDetails = data;
@@ -40,24 +38,20 @@ export default {
           this.endTimeStampRepresentation = this.addNewBusinessDayIncrementDto.businessDayIncrementDto.timeSnippetDto.endTimeStampRepresentation;
           this.isAddBusinessDayIncrementActive = this.beginTimeStampRepresentation !== null && this.endTimeStampRepresentation !== null;
           console.info("begin: " + this.beginTimeStampRepresentation + ", end: " + this.endTimeStampRepresentation + ", is active: " + this.isAddBusinessDayIncrementActive);
-      }).catch(error => console.error("Error occurred while start/stop businessDayIncrement", error));
+        }).catch(error => console.error("Error occurred while start/stop businessDayIncrement", error));
     },
     resume: function () {
       fetch(timeRecorderApiUrl + "/resume", this.postRequestOptions)
         .then(response => response.json())
         .then(data => (this.postId = data.id))
-        .catch(error => {
-          console.error("Error occurred while resuming", error);
-      });
+        .catch(error => console.error("Error occurred while resuming", error));
       this.$emit('resumed');
     },
-    book: function(){
+    book: function () {
       fetch(timeRecorderApiUrl + "/book", this.postRequestOptions)
         .then(response => response.json())
         .then(data => (this.postId = data.id))
-        .catch(error => {
-          console.error("Error occurred while booking", error);
-      });
+        .catch(error => console.error("Error occurred while booking", error));
     },
   }
 }
--- zeiterfassung-web/src/main/ui/src/views/ZeiterfassungRecordingStatus.vue
+++ zeiterfassung-web/src/main/ui/src/views/ZeiterfassungRecordingStatus.vue
@@ -2,9 +2,14 @@
   <div>
     <h2 class="centeredText">{{ title }} </h2>
     <div class="timeRecorderStatusView centered">
-      <div v-bind:class="{ isRecording: timeRecorderDto.isRecording, isNotRecording: !timeRecorderDto.isRecording}" >{{timeRecorderDto.statusMsg}}</div>
+      <div v-bind:class="{ isRecording: timeRecorderDto.isRecording, isNotRecording: !timeRecorderDto.isRecording}">
+        {{ timeRecorderDto.statusMsg }}
+      </div>
       <div class="container">
-        <button class="containerElement" :disabled="isAddBusinessDayIncrementActive" v-on:click="startStopRecordingAndRefresh">{{timeRecorderDto.isRecording ? 'Aufzeichnung beenden' : 'Aufzeichnung starten' }}</button>
+        <button class="containerElement" :disabled="isAddBusinessDayIncrementActive"
+                v-on:click="startStopRecordingAndRefresh">
+          {{ timeRecorderDto.isRecording ? 'Aufzeichnung beenden' : 'Aufzeichnung starten' }}
+        </button>
         <button class="containerElement" :disabled="isBookButtonDisabled" v-on:click="bookAndRefresh">Abbuchen</button>
       </div>
     </div>
@@ -14,54 +19,60 @@
       v-bind:initTicketNr="ticketNr"
       v-if="isAddBusinessDayIncrementActive"
       @refreshUi="refreshUiState"
-      @resumed="refreshUiState"
+      @resumed="onResumed"
     >
     </add-businessday-increment>
     <error-box v-if="postErrorDetails">
-      {{postErrorDetails}}
+      {{ postErrorDetails }}
     </error-box>
   </div>
 </template>
 
 <script>
-  import AddNewBusinessDayIncrement from '../components/AddNewBusinessDayIncrement.vue';
-  import ErrorBox from '../components/ErrorBox.vue';
-  import timeRecorderApi from '../mixins/TimeRecorderApi';
-  import appApi from '../mixins/AppApi';
-  export default {
-    name: 'ZeiterfassungRecordingStatus',
-    mixins: [timeRecorderApi, appApi],
-    components: {
-      'add-businessday-increment': AddNewBusinessDayIncrement,
-      'error-box': ErrorBox
+import AddNewBusinessDayIncrement from '../components/AddNewBusinessDayIncrement.vue';
+import ErrorBox from '../components/ErrorBox.vue';
+import timeRecorderApi from '../mixins/TimeRecorderApi';
+import appApi from '../mixins/AppApi';
+
+export default {
+  name: 'ZeiterfassungRecordingStatus',
+  mixins: [timeRecorderApi, appApi],
+  components: {
+    'add-businessday-increment': AddNewBusinessDayIncrement,
+    'error-box': ErrorBox
+  },
+  data() {
+    return {
+      title: 'Aktueller Status:',
+      isAddBusinessDayIncrementActive: false,
+      ticketNr: '',
+      beginTimeStampRepresentation: '',
+      endTimeStampRepresentation: '',
+    }
+  },
+  computed: {
+    isBookButtonDisabled: function () {
+      return !this.timeRecorderDto.isBookingPossible;
+    }
+  },
+  methods: {
+    bookAndRefresh: function () {
+      this.book();
+      this.refreshUiWithHistory();
     },
-    data() {
-      return {
-        title: 'Aktueller Status:',
-        isAddBusinessDayIncrementActive: false,
-        ticketNr: '',
-        beginTimeStampRepresentation: '',
-        endTimeStampRepresentation: '',
-      }
+    startStopRecordingAndRefresh: function () {
+      this.startStopRecording();
+      this.fetchTimeRecorderDto();// only 'refresh' this view
     },
-    computed: {
-      isBookButtonDisabled: function(){
-        return !this.timeRecorderDto.isBookingPossible;
-      }
+    onResumed: function () {
+      console.log('onResumed..');
+      this.isAddBusinessDayIncrementActive = false;
+      this.refreshUiState();
     },
-    methods: {
-      bookAndRefresh: function(){
-        this.book();
-        this.refreshUiWithHistory();
-      },
-      startStopRecordingAndRefresh: function(){
-        this.startStopRecording();
-        this.fetchTimeRecorderDto();// only 'refresh' this view
-      },
-      refreshUiState: function(){
-        console.log("ZeiterfassungRecordStatus: refreshUiStates");
-        this.requestUiRefresh();
-      },
+    refreshUiState: function () {
+      console.log("ZeiterfassungRecordStatus: refreshUiStates");
+      this.requestUiRefresh();
+    },
   },
   mounted() {
     this.fetchTimeRecorderDto();
@@ -69,15 +80,15 @@
 }
 </script>
 <style scoped>
-  .isNotRecording{
-    background-color: #ffcccb;
-    margin-bottom: 3px;
-    padding: 3px;
-  }
+.isNotRecording {
+  background-color: #ffcccb;
+  margin-bottom: 3px;
+  padding: 3px;
+}
 
-  .isRecording{
-    background-color: #90EE90;
-    margin-bottom: 3px;
-    padding: 3px;
-  }
+.isRecording {
+  background-color: #90EE90;
+  margin-bottom: 3px;
+  padding: 3px;
+}
 </style>
